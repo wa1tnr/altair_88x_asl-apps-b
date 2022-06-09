cba8b3c01bfffe402dc876e2b15269a259959eac
wa1tnr <wa1tnr@users.noreply.github.com>
2022-06-09 23:19:45 +0000

    annotate for ERZER-6 - unknown - ASL compiler

--- proj_dd/figforth.asm
+++ proj_dd/figforth.asm
@@ -3171,7 +3171,7 @@ DRZER	DW	DOCOL,ZERO
 	DB	83H	; DR1
 	DB	'DR'
 	DB	'1'+80H
-	DW	ERZER-6
+	DW	ERZER-6 ; unknown - tnr bug found 23:18z  9 June 2022 ASL compile environment
 DRONE	DW	DOCOL
 	DW	DENSTY,AT
 	DW	ZBRAN,DRON1-$
